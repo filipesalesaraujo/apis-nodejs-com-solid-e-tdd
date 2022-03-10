3a76d67d2867ba1396ad8bfc1d73ef0697a732b3
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-03-10 12:57:44 -0300

    in-memory e finalizacao

--- src/application/usecases/create-challenge-submission.spec.ts
+++ src/application/usecases/create-challenge-submission.spec.ts
@@ -1,12 +1,36 @@
 import { CreateChallengeSubmission } from "./create-challenge-submission";
 
+import { InMemoryChallengesRepository } from "../../../tests/repositories/in-memory-challenges-repository";
+import { InMemoryStudentsRepository } from "../../../tests/repositories/in-memory-students-repository";
+import { Student } from "../../domain/entities/student";
+import { Challenge } from "../../domain/entities/challenge";
+
 describe("Create challenge submission use case", () => {
   it("should be able to create a new challenge submssion", async () => {
-    const sut = new CreateChallengeSubmission();
+    const studentsRepository = new InMemoryStudentsRepository();
+    const challengesRepository = new InMemoryChallengesRepository();
+
+    const student = Student.create({
+      name: "Fulano",
+      email: "fulano@.com.br",
+    });
+
+    const challenge = Challenge.create({
+      title: "Challenge 1",
+      instructionUrl: "http://example.com",
+    });
+
+    studentsRepository.items.push(student);
+    challengesRepository.items.push(challenge);
+
+    const sut = new CreateChallengeSubmission(
+      studentsRepository,
+      challengesRepository
+    );
 
     const response = await sut.execute({
-      studentId: "fake-student-id",
-      challengeId: "fake-challenge-id",
+      studentId: student.id,
+      challengeId: challenge.id
     });
     expect(response).toBeTruthy();
   });
--- src/application/usecases/create-challenge-submission.ts
+++ src/application/usecases/create-challenge-submission.ts
@@ -1,4 +1,6 @@
 import { Submission } from "../../domain/entities/submission";
+import { ChallengesRepository } from "../repositories/ChallengesRepository";
+import { StudentsRepository } from "../repositories/StudentsRepository";
 
 type CreateChallengeSubmissionRequest = {
   studentId: string;
@@ -6,7 +8,23 @@ type CreateChallengeSubmissionRequest = {
 };
 
 export class CreateChallengeSubmission {
+  constructor(
+    private studentsRepository: StudentsRepository,
+    private challengesRepository: ChallengesRepository
+  ) {}
+
   async execute({ studentId, challengeId }: CreateChallengeSubmissionRequest) {
+    const student = await this.studentsRepository.findById(studentId);
+
+    if (!student) {
+      throw new Error("Student not found");
+    }
+
+    const challenge = await this.challengesRepository.findById(challengeId);
+
+    if (!challenge) {
+      throw new Error("Challenge not found");
+    }
     const submission = Submission.create({
       studentId,
       challengeId,
